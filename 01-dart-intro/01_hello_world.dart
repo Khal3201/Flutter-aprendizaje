fef69afe845feed93a9e43f99434fd23270dfdf3
Khaleb Haziel Luis Reyes <khalrey320@gmail.com>
2026-02-14 17:13:13 -0600

    primer ejercicio añadido

---
+++ 01-dart-intro/01_hello_world.dart
@@ -0,0 +1,12 @@
+void main(){
+  
+  // var myName = "Khal";
+  // String myName = "Khal";
+  // late final String myName = 'Khal';
+  
+    const String myName = 'Khal';
+  print("hola $myName");
+  print('Hola ${ myName.toUpperCase() } ');
+  print('Suma: ${ 1 + 1 }');
+  
+}
